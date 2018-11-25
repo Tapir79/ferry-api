ae8b3c6a8cdc65a64ac9ee85229aba637d27514f
eero3 <eero.siren@kolumbus.fi>
2018-11-25 14:26:53 +0200

    added hot reload deps, hot reload and hardcoded in-dev param

--- src/ferry_api/core.clj
+++ src/ferry_api/core.clj
@@ -1,8 +1,10 @@
 (ns ferry-api.core
   (:gen-class)
-  (:require [org.httpkit.server :as server]
+  (:use [org.httpkit.server :only [run-server]])
+  (:require [ring.middleware.reload :as reload]
             [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :refer [site]]
             ;[ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.util.json-response :as json-response]
             ;[ring.middleware.json :refer [wrap-json-response wrap-json-body]]
@@ -46,10 +48,16 @@
         :access-control-allow-headers ["Origin" "X-Requested-With" "Content-Type" "Accept"]
         :access-control-allow-methods [:get :post :put])))
 
+;;add this as env/profile param
+(def ^{:const true} in-dev? true)
+
 (defn -main
   "This is our app's entry point"
   [& args]
-  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))]
-    (server/run-server #'app {:port port})
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "8080"))
+        handler (if in-dev?
+                  (reload/wrap-reload (site #'app))
+                  (site app))]
+    (run-server handler {:port port})
     (println (str "Running webserver at http:/127.0.0.1:" port "/"))
     (flyway/reset)))

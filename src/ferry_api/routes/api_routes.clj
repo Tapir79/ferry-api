ebb5d7521fe423b51936924574f4e5934e5628a6
eero3 <eero.siren@kolumbus.fi>
2018-12-08 10:09:38 +0200

    added sockets with sente

--- src/ferry_api/routes/api_routes.clj
+++ src/ferry_api/routes/api_routes.clj
@@ -5,7 +5,7 @@
             [ring.middleware.cors :refer [wrap-cors]]
             [ferry-api.queries.timetables.timetables :as timetables]
             [ferry-api.routes.test-handlers :as handlers]
-            ))
+            [ferry-api.util.ws :as ws]))
 
 (defroutes app-routes
            (GET "/tests" [] handlers/get-handler)
@@ -18,4 +18,6 @@
            (GET "/departures/:stop" [stop] (handlers/json-handler (timetables/departures stop)))
            (GET "/linesegments" [] (handlers/json-handler (timetables/line-segments)))
            (GET "/stoproutes" [] (handlers/json-handler (timetables/stop-routes)))
+           (POST "/bookings" {body :body} (ws/broadcast-to-all-clients! body))
+           (GET "/booking-status" req (ws/ring-ajax-get-or-ws-handshake req))
            (route/not-found "The route was not found!"))
